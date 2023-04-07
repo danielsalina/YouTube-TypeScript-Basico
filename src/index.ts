e991fd199cbffe48d2f8c1da0b5460a8c8315a66
Your Name <you@example.com>
2023-04-07 17:30:32 -0300

    CLASE #5 - Tipos inferidos

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,39 @@
-const curso = 'TS';
-console.log(curso);
+// TIPOS DE DATOS EN JS
+// NUMBER
+// STRING
+// BOOLEAN
+// UNDEFINED
+// NULL
+// OBJECT
+// FUNCTION
+
+// TIPOS DE DATOS EN TS
+// ANT
+// TUPLAS
+// ENUMS
+// ARRAYS
+// NEVER
+// UNKNOWN
+// ENTRE OTROS QUE VERÉMOS A LO LARGO DEL CURSO...
+
+let nombre = 'TS';
+console.log(nombre.toLocaleLowerCase());
+
+let numero = 123_456;
+console.log(numero);
+
+let booleano = true;
+console.log(typeof booleano);
+
+let objeto = { clase: 'tipos' };
+console.log(typeof objeto);
+
+let array = [15984, 'kbknb', true];
+console.log(typeof array);
+
+let funcion = function nombre(name: string) {
+  return name;
+};
+
+const nombres = 'AGUA';
+console.log(nombres);
