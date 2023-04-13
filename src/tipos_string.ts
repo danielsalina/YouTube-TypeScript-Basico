3fbdc51b891fead4de5595955aa8452e41efe6d0
danielsalina <danielorlandosalinas935@gmail.com>
2023-04-13 20:27:02 -0300

    CLASE #9 - Tipos String

---
+++ src/tipos_string.ts
@@ -0,0 +1,12 @@
+let frase1 = 'LA VIDA ES BELLA';
+
+let mensaje =
+  'Hace mucho tiempo en una galaxia muy, muy lejana hace unos 1000 años escribieron por ahi ';
+
+mensaje = mensaje + ' ' + frase1;
+
+for (const iterator of frase1) {
+  // console.log(iterator + '---');
+}
+
+console.log(frase1.charAt(8));
